46e765f9676ac7e233be1f01c589f6cc272c9569
biblioMathis <mathisbescond@outlook.fr>
2021-02-09 09:06:53 +0100

    finite

--- apo/index.js
+++ apo/index.js
@@ -1,8 +1,13 @@
 function bom() {
 	ordi.classList.toggle("toggle");
 	hbc.style.display="none";
+	body.style.overflowY="scroll";
+}
+function retour(){
+	ordi.classList.remove("toggle");
+	hbc.style.display="flex";
+	body.style.overflowY="hidden";
 }
 
-
-
+r.addEventListener("click",retour);
 hb.addEventListener("click", bom);
